8327b0a75af88fe6885a3446c6cfd05f64f0a6b1
amanuel-lab <101940660+amanuel-lab@users.noreply.github.com>
2022-04-23 10:44:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("character");
+const btn = document.getElementById("btn");
+const url = "https://friends-quotes-api.herokuapp.com/quotes";
+btn.addEventListener("click", getQuote);
+
+function getQuote(){
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        const index=Math.floor((Math.random() *data.length));
+        console.log(index);
+          quote.innerHTML=`"${data[index].quote}"`;
+          character.innerHTML= data[index].character;
+
+    });
+
+}
