4ff2afc9ab501819e2addd56bcf4b0dcbe0ccf0c
amanuel-lab <101940660+amanuel-lab@users.noreply.github.com>
2022-05-01 19:55:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const url = "https://friends-quotes-api.herokuapp.com/quotes";
+
 const quote = document.getElementById("quote");
 const author = document.getElementById("character");
 const btn = document.getElementById("btn");
-const url = "https://friends-quotes-api.herokuapp.com/quotes";
+
 btn.addEventListener("click", getQuote);
 
 function getQuote(){
